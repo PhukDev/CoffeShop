e041ff2d708025dfeeb083206a16c9c3b6c55706
PhukDev <ksphuk2005@gmail.com>
2025-06-03 08:27:15 +0700

    section 5

--- CoffeShop/Program.cs
+++ CoffeShop/Program.cs
@@ -11,10 +11,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IShoppingCartRepository, ShoppingCartRepository>(ShoppingCartRepository.GetCart);
 builder.Services.AddDbContext<CoffeeshopDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("CoffeeShopDbContextConnection")));
+
+builder.Services.AddSession();
+builder.Services.AddHttpContextAccessor();
 var app = builder.Build();
+app.UseSession();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
